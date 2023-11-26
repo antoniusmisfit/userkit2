2dcad96f89cb6441743fbf345c700db902034b0c
Tony Agudo <antoniusmisfit@gmail.com>
2023-11-25 23:03:55 -0500

    Update adduser.bat
    Removed setting system attribute to home directory and "admin" account handling.

--- adduser.bat
+++ adduser.bat
@@ -1,17 +1,14 @@
 @echo off
-rem Add a user to the system.
+rem Add a user to the system.(Requires NANSI.SYS)
 mkdir c:\users
 mkdir c:\users\%1
-attrib +s c:\users\%1
+set /p pwrd_s=Password for %1: 
+echo %pwrd% > c:\users\%1\user.id
 echo @echo off > c:\users\%1\userexec.bat
 echo set user=%1 >> c:\users\%1\userexec.bat
 echo set home=c:\users\%1 >> c:\users\%1\userexec.bat
-if "%1"=="admin" goto MkAdmin
 echo prompt $e[1m$l%1@$p$g$e[0m >> c:\users\%1\userexec.bat
 echo User %1 created.
 goto End
 
-:MkAdmin
-echo prompt $e[31m$l%1@$p$g$e[0m >> c:\users\%1\userexec.bat
-echo Admin account created.
 :End
