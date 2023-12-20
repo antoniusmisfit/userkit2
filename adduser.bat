9803ce8b3649b1f33f49692a1df74eb9fbd71f8a
Tony Agudo <antoniusmisfit@gmail.com>
2023-12-20 13:14:07 -0500

    Update adduser.bat
    Fixed USER.ID setup.

--- adduser.bat
+++ adduser.bat
@@ -3,12 +3,11 @@ rem Add a user to the system.(Requires NANSI.SYS)
 mkdir c:\users
 mkdir c:\users\%1
 set /p pwrd_s=Password for %1: 
-echo %pwrd% > c:\users\%1\user.id
+echo %pwrd_s% > c:\users\%1\user.id
 echo @echo off > c:\users\%1\userexec.bat
 echo set user=%1 >> c:\users\%1\userexec.bat
 echo set home=c:\users\%1 >> c:\users\%1\userexec.bat
 echo prompt $e[1m$l%1@$p$g$e[0m >> c:\users\%1\userexec.bat
 echo User %1 created.
 goto End
-
 :End
